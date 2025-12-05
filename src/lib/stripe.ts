06e96107afa90e5837b65104aea4d40241e0316a
Christopher Fox Junior <wolftechinnovationsgithub@gmail.com>
2025-12-05 07:44:11 -0600

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,11 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripeSecretKey = import.meta.env.STRIPE_SECRET_KEY;
+if (!stripeSecretKey) {
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2025-02-24.acacia'
 });
 
